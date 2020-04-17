b9b017a44e8558a9c6fe40459760e09dffc70f12
Vladimir A. Ulitin <v.ulitin@mobin.me>
2020-04-17 21:03:19 +0300

    escape platform

--- src/main/java/com/infomaximum/platform/Platform.java
+++ src/main/java/com/infomaximum/platform/Platform.java
@@ -2,23 +2,38 @@ package com.infomaximum.platform;
 
 import com.infomaximum.cluster.Cluster;
 import com.infomaximum.platform.component.database.configure.DatabaseConfigure;
+import com.infomaximum.subsystems.querypool.QueryPool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Platform implements AutoCloseable {
 
+	private final static Logger log = LoggerFactory.getLogger(Platform.class);
+
 	private static volatile Platform instant;
 
+	private final Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+
 	private final DatabaseConfigure databaseConfigure;
+
 	private final Cluster cluster;
+	private final QueryPool queryPool;
 
 	private Platform(Builder builder) {
 		synchronized (Platform.class) {
 			if (instant != null) throw new IllegalStateException();
 
+			this.uncaughtExceptionHandler = builder.uncaughtExceptionHandler;
 			this.databaseConfigure = builder.databaseConfigure;
 			this.cluster = builder.clusterBuilder.build();
+			this.queryPool = new QueryPool(builder.uncaughtExceptionHandler);
 
 			instant = this;
 		}
+	}
+
+	public Thread.UncaughtExceptionHandler getUncaughtExceptionHandler() {
+		return uncaughtExceptionHandler;
 	}
 
 	public DatabaseConfigure getDatabaseConfigure() {
@@ -29,8 +44,16 @@ public class Platform implements AutoCloseable {
 		return cluster;
 	}
 
+	public QueryPool getQueryPool() {
+		return queryPool;
+	}
+
 	@Override
 	public void close() {
+		try {
+			// дождемся конца всех работ с БД, иначе может быть креш в RocksDB
+			queryPool.shutdownAwait();
+		} catch (InterruptedException ignore) {}
 		cluster.close();
 		instant = null;
 	}
@@ -42,12 +65,23 @@ public class Platform implements AutoCloseable {
 
 	public static class Builder {
 
+		private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+
 		private DatabaseConfigure databaseConfigure;
 
 		private Cluster.Builder clusterBuilder;
 
 		public Builder() {
-			clusterBuilder = new Cluster.Builder();
+
+			//default configure
+			this.uncaughtExceptionHandler = new Thread.UncaughtExceptionHandler() {
+				@Override
+				public void uncaughtException(Thread t, Throwable e) {
+					log.error("Uncaught exception", e);
+				}
+			};
+
+			this.clusterBuilder = new Cluster.Builder();
 		}
 
 		public Builder withConfig(DatabaseConfigure databaseConfigure) {
@@ -57,6 +91,11 @@ public class Platform implements AutoCloseable {
 
 		public Builder withClusterBuilder(Cluster.Builder clusterBuilder) {
 			this.clusterBuilder = clusterBuilder;
+			return this;
+		}
+
+		public Builder withUncaughtExceptionHandler(Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
+			this.uncaughtExceptionHandler = uncaughtExceptionHandler;
 			return this;
 		}
 
