c4e6d9200789c11c65cc27b1c58b04940e8f8a94
Vladimir A. Ulitin <ulitin@infomaximum.com>
2020-11-04 19:38:12 +0300

    increases version to: 0.0.1p31

---
+++ src/main/java/com/infomaximum/platform/component/frontend/engine/provider/ProviderGraphQLRequestExecuteService.java
@@ -0,0 +1,8 @@
+package com.infomaximum.platform.component.frontend.engine.provider;
+
+import com.infomaximum.platform.component.frontend.engine.service.graphqlrequestexecute.GraphQLRequestExecuteService;
+
+public interface ProviderGraphQLRequestExecuteService {
+
+    GraphQLRequestExecuteService getGraphQLRequestExecuteService();
+}
