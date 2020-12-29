04f073965196c7c9b41d3326b399b9a8f9cbc0c8
Vladimir A. Ulitin <ulitin@infomaximum.com>
2020-12-29 15:00:34 +0300

    increases version to: 0.0.1p55

--- src/main/java/com/infomaximum/platform/component/frontend/engine/controller/Controllers.java
+++ src/main/java/com/infomaximum/platform/component/frontend/engine/controller/Controllers.java
@@ -1,22 +1,33 @@
 package com.infomaximum.platform.component.frontend.engine.controller;
 
 import com.infomaximum.platform.component.frontend.engine.FrontendEngine;
-import com.infomaximum.platform.component.frontend.engine.controller.http.graphql.GraphQLController;
 
 public class Controllers {
 
-    public final Http HTTP;
+    public final Http http;
+    public final Websocket websocket;
 
     public Controllers(FrontendEngine frontendEngine) {
-        this.HTTP = new Http(frontendEngine);
+        this.http = new Http(frontendEngine);
+        this.websocket = new Websocket(frontendEngine);
     }
 
     public class Http {
 
-        public final GraphQLController graphQLController;
+        public final com.infomaximum.platform.component.frontend.engine.controller.http.graphql.GraphQLController graphQLController;
 
         public Http(FrontendEngine frontendEngine) {
-            this.graphQLController = new GraphQLController(frontendEngine);
+            this.graphQLController = new com.infomaximum.platform.component.frontend.engine.controller.http.graphql.GraphQLController(frontendEngine);
+        }
+
+    }
+
+    public class Websocket {
+
+        public final com.infomaximum.platform.component.frontend.engine.controller.websocket.graphql.GraphQLController graphQLController;
+
+        public Websocket(FrontendEngine frontendEngine) {
+            this.graphQLController = new com.infomaximum.platform.component.frontend.engine.controller.websocket.graphql.GraphQLController(frontendEngine);
         }
 
     }
