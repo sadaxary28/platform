f4b542a5dece862de48a2921da6f3912a08fac3d
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-09-30 13:06:39 +0300

    build: 0.1.3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -26,8 +26,6 @@ module com.infomaximum.platform {
     exports com.infomaximum.platform.sdk.remote.packer;
     exports com.infomaximum.platform.sdk.threadpool;
     exports com.infomaximum.platform.update.core;
-    exports com.infomaximum.platform.component.frontend.authcontext;
-    exports com.infomaximum.platform.component.frontend.context;
     exports com.infomaximum.platform.sdk.graphql.customfield.graphqlquery;
     exports com.infomaximum.platform.sdk.graphql.annotation;
     exports com.infomaximum.platform.sdk.graphql.out;
@@ -39,15 +37,12 @@ module com.infomaximum.platform {
     exports com.infomaximum.platform.sdk.utils;
     exports com.infomaximum.platform.sdk.iterator;
     exports com.infomaximum.platform.querypool;
+    exports com.infomaximum.platform.component.frontend.authcontext;
+    exports com.infomaximum.platform.component.frontend.context;
+    exports com.infomaximum.platform.component.frontend.struct;
     exports com.infomaximum.platform.component.frontend.context.source;
-    exports com.infomaximum.platform.sdk.context.source;
-    exports com.infomaximum.platform.sdk.context.source.impl;
     exports com.infomaximum.platform.component.frontend.engine.service.statistic;
     exports com.infomaximum.platform.component.frontend.request;
-    exports com.infomaximum.platform.update;
-    exports com.infomaximum.platform.update.annotation;
-    exports com.infomaximum.platform.utils;
-    exports com.infomaximum.platform.querypool.service.threadcontext;
     exports com.infomaximum.platform.component.frontend.context.source.impl;
     exports com.infomaximum.platform.component.frontend.engine;
     exports com.infomaximum.platform.component.frontend.engine.controller;
@@ -57,21 +52,26 @@ module com.infomaximum.platform {
     exports com.infomaximum.platform.component.frontend.engine.service.errorhandler;
     exports com.infomaximum.platform.component.frontend.engine.service.graphqlrequestexecute;
     exports com.infomaximum.platform.component.frontend.request.graphql.builder.impl;
-    exports com.infomaximum.platform.sdk.struct;
-    exports com.infomaximum.platform.sdk.context.impl;
-    exports com.infomaximum.platform.component.database.remote.backup;
     exports com.infomaximum.platform.component.frontend.context.impl;
     exports com.infomaximum.platform.component.frontend.engine.authorize;
     exports com.infomaximum.platform.component.frontend.utils;
-    exports com.infomaximum.platform.update.exception;
     exports com.infomaximum.platform.component.frontend.request.graphql.builder.impl.attribute;
     exports com.infomaximum.platform.component.frontend.engine.controller.http.graphql;
     exports com.infomaximum.platform.component.frontend.engine.controller.websocket.graphql;
-    exports com.infomaximum.platform.sdk.subscription;
-
-    exports com.infomaximum.platform.sdk.domainobject.module to com.infomaximum.rdao;
-
-    exports com.infomaximum.platform.sdk.graphql.datafetcher to com.infomaximum.cluster.graphql;
+    exports com.infomaximum.platform.component.database.remote.backup;
+    exports com.infomaximum.platform.component.database.remote.cfconfig;
     exports com.infomaximum.platform.component.database.remote.dbprovider to com.infomaximum.cluster;
+    exports com.infomaximum.platform.sdk.context.source;
+    exports com.infomaximum.platform.sdk.context.source.impl;
+    exports com.infomaximum.platform.update;
+    exports com.infomaximum.platform.update.annotation;
+    exports com.infomaximum.platform.utils;
+    exports com.infomaximum.platform.querypool.service.threadcontext;
+    exports com.infomaximum.platform.sdk.struct;
+    exports com.infomaximum.platform.sdk.context.impl;
+    exports com.infomaximum.platform.update.exception;
+    exports com.infomaximum.platform.sdk.subscription;
+    exports com.infomaximum.platform.sdk.domainobject.module to com.infomaximum.rdao;
+    exports com.infomaximum.platform.sdk.graphql.datafetcher to com.infomaximum.cluster.graphql;
     exports com.infomaximum.platform.sdk.dbprovider.remote to com.infomaximum.cluster;
 }
