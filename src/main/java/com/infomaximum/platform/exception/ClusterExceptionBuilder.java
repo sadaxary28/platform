13c6e6697bac1a2cd4e7ca4b902d469956aa1f2e
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-12-07 07:32:44 +0300

    build: 0.1.5p4

--- src/main/java/com/infomaximum/platform/exception/ClusterExceptionBuilder.java
+++ src/main/java/com/infomaximum/platform/exception/ClusterExceptionBuilder.java
@@ -1,5 +1,6 @@
 package com.infomaximum.platform.exception;
 
+import com.infomaximum.cluster.exception.ClusterException;
 import com.infomaximum.cluster.exception.ExceptionBuilder;
 
 import java.util.HashMap;
@@ -9,10 +10,26 @@ public class ClusterExceptionBuilder extends ExceptionBuilder {
     private static final ExceptionFactory EXCEPTION_FACTORY = new GeneralExceptionFactory();
 
     @Override
-    public PlatformException buildRemoteComponentUnavailableException(int node, int componentUniqueId, Exception cause) {
-        return EXCEPTION_FACTORY.build("remote_component_unavailable", cause, new HashMap<String, Object>() {{
-            put("node", node);
-            put("component_unique_id", componentUniqueId);
-        }});
+    public PlatformException buildTransitRequestException(int node, int componentUniqueId, String rControllerClassName, String methodName, Exception cause) {
+        return EXCEPTION_FACTORY.build(
+                "remote_component_transit_request",
+                "node: " + node + ", componentUniqueId: " + componentUniqueId + ", rControllerClassName: " + rControllerClassName + ", methodName: " + methodName,
+                new HashMap<String, Object>() {{
+                    put("node", node);
+                }},
+                cause
+        );
+    }
+
+    @Override
+    public PlatformException buildRemoteComponentUnavailableException(int node, int componentUniqueId, String rControllerClassName, String methodName, Exception cause) {
+        return EXCEPTION_FACTORY.build(
+                "remote_component_unavailable",
+                "node: " + node + ", componentUniqueId: " + componentUniqueId + ", rControllerClassName: " + rControllerClassName + ", methodName: " + methodName,
+                new HashMap<String, Object>() {{
+                    put("node", node);
+                }},
+                cause
+        );
     }
 }
